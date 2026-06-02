f9f6ffba610df7d878c99a18bcb6ab74dc0384d8
r1125824 <gustaveesteban.mougenot@student.kuleuven.be>
2026-06-02 23:17:03 +0200

    basic predator, with pointer problem when we eat a fish

---
+++ _alp/Agents/A_Predator/Code/Events.java
@@ -0,0 +1,7 @@
+void lifeCycle()
+{/*ALCODESTART::1780433027831*/
+IV_energy -= 2;
+IV_energy = max(0, IV_energy);
+traceln("Energy: "+IV_energy);
+/*ALCODEEND*/}
+
---
+++ _alp/Agents/A_Predator/Code/Functions.java
@@ -0,0 +1,19 @@
+A_Fish nearestFishNaive()
+{/*ALCODESTART::1780433174134*/
+
+A_Fish nearest = null;
+if (main != null)
+{
+	double bestDist = Double.MAX_VALUE;
+	
+	for (A_Fish f : main.fishPopulation) {
+        double d = distanceTo(f);
+        if (d < bestDist) {
+            bestDist = d;
+            nearest = f;
+	    }
+	}
+}
+return nearest;
+/*ALCODEEND*/}
+
