d4560857c10aa3b925040aef6da4731c5fe95b4f
r1125824 <gustaveesteban.mougenot@student.kuleuven.be>
2026-06-03 11:02:15 +0200

    fix vision test

--- _alp/Agents/A_Predator/Code/Events.java
+++ _alp/Agents/A_Predator/Code/Events.java
@@ -1,11 +1,13 @@
 void lifeCycle()
 {/*ALCODESTART::1780433027831*/
-setIV_energy -= 2;
+IV_energy -= 2;
 IV_energy = max(0, IV_energy);
 
-if (IV_energy <= 10) {
-	sharkImage.setVisible(IV_energy%2 == 0);
-}
+/*if (IV_energy <= 10) {
+	sharkImage.setVisible(!sharkImage.isVisible());
+} else {
+	sharkImage.setVisible(true);
+}*/
 if (IV_energy <= 0){
 	main.remove_predatorPopulation(this);
 }
