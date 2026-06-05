588e2e4709f7558f729b47d15215df84f341af0d
r1125824 <gustaveesteban.mougenot@student.kuleuven.be>
2026-06-05 13:30:52 +0200

    prototype: food as agent + ship refactor

--- _alp/Agents/Main/Code/Events.java
+++ _alp/Agents/Main/Code/Events.java
@@ -1,47 +1,13 @@
 void ShipSinks()
 {/*ALCODESTART::1779708162939*/
-shipClass.setVisible(true);
-shipActive = true;
-double shipW = 120;
-double shipH = 100;
+//shipActive = true;
+shipZone.setVisible(true); // shipNode IS the visual now
 
-double shipX = oceanNode.getX() + shipW+ uniform(0, oceanNode.getWidth() -  2* shipW);
-double shipY = oceanNode.getY() + shipH+ uniform(0, oceanNode.getHeight() - 2* shipH);
+spawnRates.add(20.);
+spawnNodes.add(shipZone);
 
-//Point p = oceanNode.randomPointInside();
-shipClass.setPos(shipX, shipY);
-double shipCenterX = shipX + shipW / 2;
-double shipCenterY = shipY + shipH / 2;
-double contamRadius = 350;
-double killRadius = 200;
-//works -> one food target gets slowly back to it's orginal -> fishes do not go there
-for (ShapeRectangle f : food_Nodes) {
-    double foodCenterX = f.getX() + f.getWidth() / 2;
-    double foodCenterY = f.getY() + f.getHeight() / 2;
+totalSpawnRate = spawnRates.stream().mapToDouble(Double::doubleValue).sum();
 
-    double d = Math.sqrt(
-        Math.pow(foodCenterX - shipCenterX, 2) +
-        Math.pow(foodCenterY - shipCenterY, 2)
-    );
-	//double foodRadiusApprox = Math.max(f.getWidth(), f.getHeight()) / 2;
-	
-    if (d <= contamRadius) {
-        contaminatedFood.add(f);
-    }
-}
-//initial killing impact -> seems to work as well
-for ( int i = fishPopulation.size() -1; i >= 0; i--) {
-	A_Fish fi = fishPopulation.get(i);
-	//double fishCenterX = fi.getX() + fi.getWidth() /2;
-	//double fishCenterY = fi.getY() + fi.getHeight() /2;
-	double d = Math.sqrt(
-        Math.pow(fi.getX() - shipCenterX, 2) +
-        Math.pow(fi.getY() - shipCenterY, 2)
-    );
-    if (d <= killRadius) {
-        remove_fishPopulation(fi);
-    }
-}
 /*ALCODEEND*/}
 
 void food_growth()
@@ -91,6 +57,25 @@ for (A_Fish f : fishPopulation) {
     if (f.V_type == 1) type1count++;
     if (f.V_type == 2) type2count++;
     if (f.V_type == 3) type3count++;
+}
+
+/*ALCODEEND*/}
+
+void spawnFood()
+{/*ALCODESTART::1780647158930*/
+if (spawnNodes.isEmpty()) return;
+
+double total = totalSpawnRate;
+double roll = uniform(0, total);
+double cumul = 0;
+
+for (int i = 0; i < spawnNodes.size(); i++) {
+    cumul += spawnRates.get(i);
+    if (roll < cumul) {
+        A_Food food = add_foodPopulation();
+        food.jumpTo(spawnNodes.get(i));
+        return;
+    }
 }
 
 /*ALCODEEND*/}
