66f3edb62e9da8e5d068ad452b4bfbce613bbcaf
r1125824 <gustaveesteban.mougenot@student.kuleuven.be>
2026-06-07 21:30:15 +0200

    aborted try with agent-based food

--- _alp/Agents/Main/Code/Events.java
+++ _alp/Agents/Main/Code/Events.java
@@ -1,13 +1,50 @@
 void ShipSinks()
 {/*ALCODESTART::1779708162939*/
-//shipActive = true;
-shipZone.setVisible(true); // shipNode IS the visual now
+/*shipClass.setVisible(true);
+shipActive = true;
+double shipW = 120;
+double shipH = 100;
 
 spawnRates.add(5.);
 spawnNodes.add(shipZone);
 
 totalSpawnRate = spawnRates.stream().mapToDouble(Double::doubleValue).sum();
 
+
+//Point p = oceanNode.randomPointInside();
+shipClass.setPos(shipX, shipY);
+double shipCenterX = shipX + shipW / 2;
+double shipCenterY = shipY + shipH / 2;
+double contamRadius = 350;
+double killRadius = 200;
+//works -> one food target gets slowly back to it's orginal -> fishes do not go there
+for (ShapeRectangle f : food_Nodes) {
+    double foodCenterX = f.getX() + f.getWidth() / 2;
+    double foodCenterY = f.getY() + f.getHeight() / 2;
+
+    double d = Math.sqrt(
+        Math.pow(foodCenterX - shipCenterX, 2) +
+        Math.pow(foodCenterY - shipCenterY, 2)
+    );
+	//double foodRadiusApprox = Math.max(f.getWidth(), f.getHeight()) / 2;
+	
+    if (d <= contamRadius) {
+        contaminatedFood.add(f);
+    }
+}
+//initial killing impact -> seems to work as well
+for ( int i = fishPopulation.size() -1; i >= 0; i--) {
+	A_Fish fi = fishPopulation.get(i);
+	//double fishCenterX = fi.getX() + fi.getWidth() /2;
+	//double fishCenterY = fi.getY() + fi.getHeight() /2;
+	double d = Math.sqrt(
+        Math.pow(fi.getX() - shipCenterX, 2) +
+        Math.pow(fi.getY() - shipCenterY, 2)
+    );
+    if (d <= killRadius) {
+        remove_fishPopulation(fi);
+    }
+}*/
 /*ALCODEEND*/}
 
 void food_growth()
